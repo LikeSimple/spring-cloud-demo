0b660db74cd25495b9928f97a3623244afbdff51
Jimmy XU <wolfxz@foxmail.com>
2017-08-29 19:47:12 +0800

    add SSO service project basic demo

---
+++ service-sso/src/main/java/com/newtouch/cloud/servicesso/ServiceSsoApplication.java
@@ -0,0 +1,15 @@
+package com.newtouch.cloud.servicesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class ServiceSsoApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSsoApplication.class, args);
+    }
+}
