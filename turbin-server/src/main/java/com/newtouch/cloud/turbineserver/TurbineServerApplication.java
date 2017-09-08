9657b37ac24ee8bc14428db3b432b5c9576fb8f7
Jimmy XU <wolfxz@foxmail.com>
2017-09-08 22:52:08 +0800

    update sleuth-server use mysql and turbine-server use stream to gather metrics

--- turbin-server/src/main/java/com/newtouch/cloud/turbineserver/TurbineServerApplication.java
+++ turbin-server/src/main/java/com/newtouch/cloud/turbineserver/TurbineServerApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrixDashboard
-@EnableTurbine
+@EnableTurbineStream
 public class TurbineServerApplication {
 
     public static void main(String[] args) {
