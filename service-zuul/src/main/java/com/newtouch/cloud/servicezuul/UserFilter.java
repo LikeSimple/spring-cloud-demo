8a0b8e7a969e791bd3969b6784ea1d1c4d188a81
徐瑱 <zhen.xu@newtouch.cn>
2017-10-30 10:23:36 +0800

    added dockerfile maven plugin updated version from 0.1 to 0.9

---
+++ service-zuul/src/main/java/com/newtouch/cloud/servicezuul/UserFilter.java
@@ -0,0 +1,36 @@
+package com.newtouch.cloud.servicezuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Log4j2
+public class UserFilter extends ZuulFilter {
+
+  @Override
+  public String filterType() {
+    return "pre";
+  }
+
+  @Override
+  public int filterOrder() {
+    return 0;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+
+  @Override
+  public Object run() {
+    RequestContext ctx = RequestContext.getCurrentContext();
+    HttpServletRequest request = ctx.getRequest();
+    //把用户信息加入请求头部
+    ctx.addZuulRequestHeader("X-AUTH-ID", request.getUserPrincipal().getName());
+
+    return null;
+  }
+}
